cd1e7d79a83360f918f2bfbc3dcaccfae1fc94ce
James Greiner <jamesdgreiner@icloud.com>
2021-04-11 17:24:25 -0700

    Added 'R' requirement for CRUD capabilities in User

--- SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
+++ SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
@@ -1,12 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using SecretSanta.Web.ViewModels;
 
 namespace SecretSanta.Web.Controllers
 {
     public class UsersController : Controller
     {
+        static List<UserViewModel> Users = new List<UserViewModel>
+        {
+            new UserViewModel{FirstName = "John ", LastName="Smith"},
+            new UserViewModel{FirstName = "Jane ", LastName="Smith"}
+        };
         public IActionResult Index()
         {
-            return View();
+            return View(Users);
         }
     }
 }
---
+++ SecretSanta/src/SecretSanta.Web/ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+namespace SecretSanta.Web.ViewModels
+{
+    public class UserViewModel
+    {
+        public string FirstName { get; set; } = "";
+
+        public string LastName { get; set; } = "";
+    }
+}
