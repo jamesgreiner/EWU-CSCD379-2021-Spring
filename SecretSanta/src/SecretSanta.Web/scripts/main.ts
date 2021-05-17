eb9c9fe16cfdffbf8c294410ac89c6daa73429da
James Greiner <jamesdgreiner@icloud.com>
2021-05-16 23:21:43 -0700

    Started Assignment 7

--- SecretSanta/src/SecretSanta.Web/scripts/main.ts
+++ SecretSanta/src/SecretSanta.Web/scripts/main.ts
@@ -1,7 +1,6 @@
 import '../styles/site.css';
-
 import 'alpinejs';
-
+import axios from 'axios';
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
---
+++ SecretSanta/test/SecretSanta.E2E.Tests/EndToEndTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecretSanta.E2E.Tests
+{
+    [TestClass]
+    public class EndToEndTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
