851835d7d0d1d6c656a58ac91afbc5619ae09e4b
James Greiner <jamesdgreiner@icloud.com>
2021-05-18 21:32:35 -0700

    Added automated tests per requirements, updated crud pages

--- SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
+++ SecretSanta/src/SecretSanta.Web/Controllers/UsersController.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using SecretSanta.Web.Api;
-using SecretSanta.Web.ViewModels;
 
 namespace SecretSanta.Web.Controllers
 {
@@ -11,77 +8,32 @@ namespace SecretSanta.Web.Controllers
     {
         public IUsersClient UserClient { get; }
 
+
+
         public UsersController(IUsersClient userClient)
         {
             UserClient = userClient ?? throw new ArgumentNullException(nameof(userClient));
         }
 
-        public async Task<IActionResult> Index()
+
+
+        public IActionResult Index()
         {
-            var users = await UserClient.GetAllAsync();
-            var userViewModels = users.Select(x => new UserViewModel
-            {
-                Id = x.Id,
-                FirstName = x.FirstName,
-                LastName = x.LastName
-            }).ToList();
-            return View(userViewModels);
+            return View();
         }
+
+
 
         public IActionResult Create()
         {
             return View();
         }
 
-        [HttpPost]
-        public async Task<IActionResult> Create(UserViewModel viewModel)
+    
+
+        public IActionResult Edit(int id)
         {
-            if (ModelState.IsValid)
-            {
-                await UserClient.PostAsync(new User
-                {
-                    Id = viewModel.Id,
-                    FirstName = viewModel.FirstName,
-                    LastName = viewModel.LastName
-                });
-                return RedirectToAction(nameof(Index));
-            }
-
-            return View(viewModel);
-        }
-
-        public async Task<IActionResult> Edit(int id)
-        {
-            User user = await UserClient.GetAsync(id);
-            return View(new UserViewModel
-            {
-                Id = user.Id,
-                FirstName = user.FirstName,
-                LastName = user.LastName
-            });
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Edit(UserViewModel viewModel)
-        {
-            if (ModelState.IsValid)
-            {
-                await UserClient.PutAsync(viewModel.Id, new UpdateUser
-                {
-                    FirstName = viewModel.FirstName,
-                    LastName = viewModel.LastName
-                });
-                return RedirectToAction(nameof(Index));
-            }
-
-            return View(viewModel);
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Delete(int id)
-        {
-            await UserClient.DeleteAsync(id);
-            return RedirectToAction(nameof(Index));
+            return View();
         }
     }
 }
--- SecretSanta/test/SecretSanta.E2E.Tests/EndToEndTests.cs
+++ SecretSanta/test/SecretSanta.E2E.Tests/EndToEndTests.cs
@@ -1,13 +1,216 @@
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlaywrightSharp;
+using System.Linq;
 
 namespace SecretSanta.E2E.Tests
 {
     [TestClass]
     public class EndToEndTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private static WebHostServerFixture<Web.Startup, UserGroup.Api.Startup> Server;
+
+        [ClassInitialize]
+        public static void InitializeClass(TestContext testContext)
         {
+            Server = new();
+        }
+
+        // This test checks that 'Secret Santa' is diplayed in the navigation banner when on the homepage
+        [TestMethod]
+        public async Task LaunchHomepage()
+        {
+            var localhost = Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync(localhost);
+
+            Assert.IsTrue(response.Ok);
+
+            var headerContent = await page.GetTextContentAsync("body > header > div > a");
+            Assert.AreEqual("Secret Santa", headerContent);
+        }
+
+
+
+        // Test for navigating to the Users page
+        [TestMethod]
+        public async Task NavigateToUsers()
+        {
+            var localhost = Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync(localhost);
+
+            Assert.IsTrue(response.Ok);
+
+            await page.ClickAsync("text=Users");
+            await page.ScreenshotAsync("users.png");
+        }
+
+
+
+        // Test for navigating to the Groups page
+        [TestMethod]
+        public async Task NavigateToGroups()
+        {
+            var localhost = Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync(localhost);
+
+            Assert.IsTrue(response.Ok);
+
+            await page.ClickAsync("text=Groups");
+            await page.ScreenshotAsync("groups.png");
+        }
+
+
+
+        // Test for navigating to the Gifts page
+        [TestMethod]
+        public async Task NavigateToGifts()
+        {
+            var localhost = Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync(localhost);
+
+            Assert.IsTrue(response.Ok);
+
+            await page.ClickAsync("text=Gifts");
+
+            await page.ScreenshotAsync("gifts.png");
+        }
+
+
+
+        // Test that a gift can be added and the total number of gifts increases by one
+        [TestMethod]
+        public async Task CreateGift()
+        {
+            var localhost = Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync(localhost);
+
+            Assert.IsTrue(response.Ok);
+
+            await page.ClickAsync("text=Gifts");
+
+            var gifts = await page.QuerySelectorAllAsync("body > section > section > section");
+            int currentNumberOfGifts = gifts.Count();
+
+            // open the create page
+            await page.ClickAsync("text=Create");
+            
+            await page.TypeAsync("input#Title", "Lego Millenium Falcon");
+            await page.TypeAsync("input#Description", "This is a lego replica of the Millenium Falcon from Star Wars.");
+            await page.TypeAsync("input#Url", "https://www.lego.com/en-us/product/millennium-falcon-75192");
+            await page.TypeAsync("input#Priority", "1");
+            await page.SelectOptionAsync("select#UserId", "2");
+        
+            // create the gift
+            await page.ClickAsync("text=Create");
+
+            // ensure the number of gifts increased by one
+            gifts = await page.QuerySelectorAllAsync("body > section > section > section");
+            int newNumberOfGifts = gifts.Count();
+            Assert.AreEqual(newNumberOfGifts, currentNumberOfGifts + 1);
+        }
+
+
+
+        // This will ensure the last gift in the list can be updated
+        [TestMethod]
+        public async Task UpdateLastGift() 
+        {
+            var localhost = Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions 
+            {
+                Headless = true
+            });
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync(localhost);
+
+            Assert.IsTrue(response.Ok);
+
+            // go to Gifts page
+            await page.ClickAsync("text=Gifts");
+
+            // grab last gift
+            await page.ClickAsync("body > section > section > section:last-child");
+
+            // select all text
+            await page.ClickAsync("input#Title", clickCount:3);
+
+            // fill in new title of gift
+            await page.TypeAsync("input#Title", "Updated Gift");
+
+            // update gift
+            await page.ClickAsync("text=Update");
+
+            await page.WaitForSelectorAsync("body > section > section > section:last-child");
+            var giftContent = await page.GetTextContentAsync("body > section > section > section:last-child");
+            
+            Assert.IsTrue(giftContent.Contains("Updated Gift"));
+        }
+
+
+
+        [TestMethod]
+        public async Task DeleteLastGift()
+        {
+            var localhost = Server.WebRootUri.AbsoluteUri.Replace("127.0.0.1", "localhost");
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+
+            var page = await browser.NewPageAsync();
+            var response = await page.GoToAsync(localhost);
+
+            Assert.IsTrue(response.Ok);
+
+            await page.ClickAsync("text=Gifts");
+            
+            var gifts = await page.QuerySelectorAllAsync("body > section > section > section");
+            int currentNumberOfGifts = gifts.Count();
+
+            page.Dialog += (_, args) => args.Dialog.AcceptAsync();
+
+            await page.ClickAsync("body > section > section > section:last-child > a > section > form > button");
+            gifts = await page.QuerySelectorAllAsync("body > section > section > section");
+            int newNumberOfGifts = gifts.Count();
+            Assert.AreEqual(newNumberOfGifts, currentNumberOfGifts - 1);
         }
     }
 }
